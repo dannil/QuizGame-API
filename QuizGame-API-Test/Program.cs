bdabc4b6b1187311da90800aa6698a1e2dd45ca8
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-12-20 19:38:56 +0100

    Added project for NUnit testing

---
+++ QuizGame-API-Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using QuizGameAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuizGame_API_Test
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            API api = new API("");
+            Question question = api.GetQuestionByID(1);
+        }
+    }
+}
---
+++ QuizGame-API-Test/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using QuizGameAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizGame_API_Test
+{
+    public class Test
+    {
+        [TestCase]
+        public void GetQuestionByID()
+        {
+            API api = new API("");
+            api.GetQuestionByID(1);
+        }
+    }
+}
