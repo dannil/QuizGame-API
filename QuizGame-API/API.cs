6cb39fb40f3260fc62cde10d4d094aca84a74fbd
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-12-22 11:17:19 +0100

    Added documentation

--- QuizGame-API/API.cs
+++ QuizGame-API/API.cs
@@ -32,6 +32,10 @@ namespace QuizGameAPI
 
         // ----- CATEGORIES ----- //
 
+        /// <summary>
+        /// Get a list of all categories.
+        /// </summary>
+        /// <returns>A list of categories</returns>
         public List<CategoryHolder> GetCategories()
         {
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url + "/category");
@@ -60,6 +64,11 @@ namespace QuizGameAPI
 
         // ----- QUESTIONS ----- //
 
+        /// <summary>
+        /// Get a question with the specified id.
+        /// </summary>
+        /// <param name="id">The id of the question</param>
+        /// <returns>A question with the specified id</returns>
         public Question GetQuestion(int id)
         {
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url + "/question/" + id);
@@ -86,6 +95,10 @@ namespace QuizGameAPI
             }
         }
 
+        /// <summary>
+        /// Get a list of all questions.
+        /// </summary>
+        /// <returns>A list of questions</returns>
         public List<Question> GetQuestions()
         {
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url + "/question");
@@ -112,12 +125,17 @@ namespace QuizGameAPI
             }
         }
 
+        /// <summary>
+        /// Get a list of all questions which contains the specified category.
+        /// </summary>
+        /// <param name="category">The category of the questions</param>
+        /// <returns>A list of questions</returns>
         public List<Question> GetQuestionsByCategory(String category)
         {
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url + "/question/category/" + category);
+            request.Method = "GET";
             try
             {
-                request.Method = "GET";
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 HttpStatusCode statusCode = response.StatusCode;
                 if (statusCode.Equals(HttpStatusCode.OK))
@@ -138,6 +156,11 @@ namespace QuizGameAPI
             }
         }
 
+        /// <summary>
+        /// Delete a question with the specified id
+        /// </summary>
+        /// <param name="id">The id of the question</param>
+        /// <returns>True if a question with that id exists and is successfully deleted, otherwise false</returns>
         public Boolean DeleteQuestion(int id)
         {
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url + "/question/" + id);
