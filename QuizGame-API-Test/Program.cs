cb9bf5c1c20a78ad946057b8cc79f5c5b20932a6
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-12-22 12:37:07 +0100

    Added tests

--- QuizGame-API-Test/Program.cs
+++ QuizGame-API-Test/Program.cs
@@ -11,10 +11,15 @@ namespace QuizGame_API_Test
         public static void Main(String[] args)
         {
             API api = new API("");
-            Question question = api.GetQuestionByID(0);
+            Question question = api.GetQuestion(0);
             List<Question> questions1 = api.GetQuestions();
             List<Question> questions2 = api.GetQuestionsByCategory("algebra");
             List<CategoryHolder> categories1 = api.GetCategories();
+            Boolean success = api.DeleteQuestion(0);
+
+            Question question3 = new Question();
+            question3.Title = "Testing post";
+            api.AddQuestion(question3);
         }
     }
 }
--- QuizGame-API-Test/Test.cs
+++ QuizGame-API-Test/Test.cs
@@ -24,7 +24,7 @@ namespace QuizGame_API_Test
         public void GetQuestionByID()
         {
             API api = new API("");
-            Question question = api.GetQuestionByID(1);
+            Question question = api.GetQuestion(1);
             Assert.AreNotEqual(question, null);
         }
 
@@ -42,6 +42,23 @@ namespace QuizGame_API_Test
             API api = new API("");
             List<Question> questions = api.GetQuestionsByCategory("basic");
             Assert.AreNotEqual(questions, null);
+        }
+
+        [TestCase]
+        public void AddQuestion()
+        {
+            API api = new API("");
+            Question question = new Question();
+            question.Title = "testing post";
+            api.AddQuestion(question);
+        }
+
+        [TestCase]
+        public void DeleteQuestion()
+        {
+            API api = new API("");
+            Boolean success = api.DeleteQuestion(0);
+            Assert.AreEqual(success, true);
         }
     }
 }
