e55ca1ac122ccb0d6a92b0419b9360b5db851221
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-01-08 13:59:52 +0100

    Added tests and logic for local testing of questions and null values

--- QuizGame-API-Test/Test.cs
+++ QuizGame-API-Test/Test.cs
@@ -15,6 +15,11 @@ namespace QuizGame_API_Test
         public void TestURLConstructor()
         {
             API api = new API("http://localhost:8080/quizgame-backend", "api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
+            
+            Question question = new Question("Solve 4 + 4 * 4");
+            question.AddCategories("basic");
+            question.AddAnswers("16", "20", "24");
+            api.AddQuestion(question);
 
             List<String> categories = api.GetCategories();
 
@@ -193,7 +198,7 @@ namespace QuizGame_API_Test
         /// Test-ID 6
         /// </summary>
         [TestCase]
-        public void EditQuestionTitle()
+        public void EditQuestionTitleDatabase()
         {
             API api = new API("api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
 
@@ -212,7 +217,7 @@ namespace QuizGame_API_Test
         /// Test-ID 8
         /// </summary>
         [TestCase]
-        public void EditQuestionCategoriesAdd()
+        public void EditQuestionCategoriesAddDatabase()
         {
             API api = new API("api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
 
@@ -231,7 +236,7 @@ namespace QuizGame_API_Test
         /// Test-ID XX
         /// </summary>
         [TestCase]
-        public void EditQuestionCategoriesRemove()
+        public void EditQuestionCategoriesRemoveDatabase()
         {
             API api = new API("api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
 
@@ -251,7 +256,7 @@ namespace QuizGame_API_Test
         /// Test-ID 12
         /// </summary>
         [TestCase]
-        public void EditQuestionAnswersAdd()
+        public void EditQuestionAnswersAddDatabase()
         {
             API api = new API("api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
 
@@ -273,7 +278,7 @@ namespace QuizGame_API_Test
         /// Test-ID 13
         /// </summary>
         [TestCase]
-        public void EditQuestionAnswersRemove()
+        public void EditQuestionAnswersRemoveDatabase()
         {
             API api = new API("api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
 
@@ -295,7 +300,7 @@ namespace QuizGame_API_Test
         /// Test-ID XX
         /// </summary>
         [TestCase]
-        public void EditQuestionCorrect()
+        public void EditQuestionCorrectDatabase()
         {
             API api = new API("api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
 
@@ -429,5 +434,191 @@ namespace QuizGame_API_Test
 
             Assert.AreNotEqual(question.ToString(), question2.ToString());
         }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionID()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+            question.ID = 0;
+
+            Question question2 = QuestionUtility.GetGenericQuestion();
+            question2.ID = 1;
+
+            Assert.AreNotEqual(question.ID, question2.ID);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionTitle()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+            question.Title = "title";
+
+            Question question2 = QuestionUtility.GetGenericQuestion();
+            question2.Title = "another title";
+
+            Assert.AreNotEqual(question.Title, question2.Title);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionCategoriesAdd()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+            question.AddCategories("generic");
+
+            Question question2 = QuestionUtility.GetGenericQuestion();
+            question2.AddCategories("generic", "undefined");
+
+            Assert.AreNotEqual(question.Categories, question2.Categories);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionCategoriesRemove()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+            question.AddCategories("undefined");
+
+            Question question2 = QuestionUtility.GetGenericQuestion();
+            question2.AddCategories("undefined");
+            question2.RemoveCategories("undefined");
+
+            Assert.AreNotEqual(question.Categories, question2.Categories);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionAnswersAdd()
+        {
+            Question question = new Question("Solve 3 + 5 * 10");
+            question.AddCategories("basic");
+            question.AddAnswers("80", "53", "35");
+            question.Correct = "53";
+
+            Question question2 = new Question("Solve 3 + 5 * 10");
+            question2.AddCategories("basic");
+            question2.AddAnswers("80", "53", "35", "200");
+            question2.Correct = "53";
+
+            Assert.AreNotEqual(question.Answers, question2.Answers);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionAnswersRemove()
+        {
+            Question question = new Question("Solve 3 + 5 * 10");
+            question.AddCategories("basic");
+            question.AddAnswers("80", "53", "35");
+            question.Correct = "53";
+
+            Question question2 = new Question("Solve 3 + 5 * 10");
+            question2.AddCategories("basic");
+            question2.AddAnswers("80", "53", "35");
+            question2.Correct = "53";
+
+            question2.RemoveAnswers("80");
+
+            Assert.AreNotEqual(question.Answers, question2.Answers);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionCorrect()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+            question.Correct = "changed";
+
+            Question question2 = QuestionUtility.GetGenericQuestion();
+            question2.Correct = "another correct";
+
+            Assert.AreNotEqual(question.Correct, question2.Correct);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionIDLowerBoundary()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+            question.ID = -1;
+
+            // ID defaults to 0 if it's value isn't valid
+            Assert.AreEqual(question.ID, 0);
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionTitleNull()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                question.Title = null;
+            });
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionCategoryNull()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                question.AddCategories(null);
+            });
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionAnswerNull()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                question.AddAnswers(null);
+            });
+        }
+
+        /// <summary>
+        /// Test-ID XX
+        /// </summary>
+        [TestCase]
+        public void EditQuestionCorrectNull()
+        {
+            Question question = QuestionUtility.GetGenericQuestion();
+
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                question.Correct = null;
+            });
+        }
+
     }
 }
