83936a9ef8e72e43e2a473c3c0117c4b9be1c26b
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-01-15 23:38:17 +0100

    Added demo project

---
+++ QuizGame-API-Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.API.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            API api = new API("api-test", "2fb5e13419fc89246865e7a324f476ec624e8740");
+
+            // Add a question
+            Console.WriteLine("Press enter to run add question-demo: ");
+            Console.ReadLine();
+
+            Question question = new Question("Solve a, where a + 4 = 11");
+            question.AddCategories("basic", "algebra");
+            question.AddAnswers("Undefined", "0", "-7", "7");
+            question.Correct = "7";
+
+            Question result = api.AddQuestion(question);
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+
+            // Delete a question
+            Console.WriteLine("Press enter to run delete question-demo: ");
+            Console.ReadLine();
+
+            Boolean success = api.DeleteQuestion(result.ID);
+            Console.WriteLine(success);
+
+            Console.ReadLine();
+        }
+    }
+}
